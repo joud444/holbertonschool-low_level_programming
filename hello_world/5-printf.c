5be5c0175610a895e5904fc11d1ba070be18fc67
“joud444” <joudalnasser3@gmail.com>
2025-10-27 13:09:06 +0000

    k

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,13 @@
-include <stdio.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
 int main(void)
 {
-printif("with proper grammar, but the outcomr is piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
