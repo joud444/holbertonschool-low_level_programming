d760117be0187d0945b26e72282aaee13151acea
“joud444” <joudalnasser3@gmail.com>
2025-11-04 11:08:51 +0000

    fix: Betty style compliance for _islower function

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,16 +1,19 @@
 #include "main.h"
+
 /**
- * here is my thered try
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-int _islower (int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-
-else 
-{
-	return (0); 
-}
+	else
+	{
+		return (0);
+	}
 }
