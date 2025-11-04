4193bae2ab8046e2d98c31fb401efe21ab2d7fdd
“joud444” <joudalnasser3@gmail.com>
2025-11-04 10:11:19 +0000

    Add 2-print_alphabet_x10.c function

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * here is my sce try
+ *
+ */
+
+void print_alphabet_x10(void);
+{
+	char letter;
+	int i;
+
+	for (i = 0; <10 i; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter ++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
