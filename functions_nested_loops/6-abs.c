4d75b1eddcc303f85fabe55611ab261e87f559df
“joud444” <joudalnasser3@gmail.com>
2025-11-10 12:44:04 +0000

    feat: Implement _abs function to compute absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,13 +1,19 @@
 #include "main.h"
+
 /**
- * new week
+ * _abs - computes the absolute value of an integer
+ * @n: the integer to compute absolute value for
+ *
+ * Return: the absolute value of n
  */
 int _abs(int n)
 {
 	if (n < 0)
+	{
 		return (-n);
-	else 
+	}
+	else
+	{
 		return (n);
+	}
 }
-
-
