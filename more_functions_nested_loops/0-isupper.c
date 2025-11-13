36206d8a92c46d72c2768ef9b283c23782489154
joud444 <joudalnasser3@gmail.com>
2025-11-13 21:09:06 +0000

    fix: Add Betty-compliant documentation to _isupper function

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
