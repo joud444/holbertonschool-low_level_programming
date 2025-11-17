48be4bbe1adb6665ccb61c2d903830d4764225bd
“joud444” <joudalnasser3@gmail.com>
2025-11-17 14:30:44 +0000

    Task 2: Implement mul function that multiplies two integers

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,9 +4,10 @@
  * mul - multiplies two integers
  * @a: first integer
  * @b: second integer
+ *
  * Return: product of a and b
  */
 int mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
